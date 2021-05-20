28a5509dbd89dbb59ef02a438dcea676b89cd311
steve <pxxonline@gmail.com>
2021-05-20 15:02:02 +0200

    refactor: update UserVotes component test

--- client/src/poll/component/UserVotes.test.tsx
+++ client/src/poll/component/UserVotes.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import { IVote } from '../api/poll.interface';
 import { UserVotes } from './UserVotes';
 
-// todo: rewrite this after styling
 test('renders users and votes', () => {
   const MOCK_VOTES: IVote[] = [
     { user: { id: '1', name: 'bob' }, vote: '1' },
@@ -12,6 +11,15 @@ test('renders users and votes', () => {
 
   render(<UserVotes votes={MOCK_VOTES} />);
 
-  const bob = screen.getByText(/bob (1)/i);
+  const bob = screen.getByText(/bob/i);
   expect(bob).toBeInTheDocument();
+
+  const bobVote = screen.getByText(/1/i);
+  expect(bobVote).toBeInTheDocument();
+
+  const alice = screen.getByText(/alice/i);
+  expect(alice).toBeInTheDocument();
+
+  const aliceVote = screen.getByText(/2/i);
+  expect(aliceVote).toBeInTheDocument();
 });
